60b6bdfe9a10d30ca9e6dae3dc48729135ebccd4
Ermolenko.K <19symbols@gmail.com>
2021-01-17 19:56:47 +0300

    Обработка ошибок. Вывод предупреждения (Alert) при возникновении ошибки

--- SwiftUI-Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ SwiftUI-Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @StateObject private var viewModel = AppetizerListViewModel()
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(viewModel.appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear {
+            viewModel.getAppetizers()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
---
+++ SwiftUI-Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  AppetizerListViewModel.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Ермоленко Константин on 17.01.2021.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+
+    @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { [self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    switch error {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
