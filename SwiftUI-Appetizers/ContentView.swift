b24fd28a69d99ee448c67f7cb62a4e76c9d8dea6
Ermolenko.K <19symbols@gmail.com>
2021-01-17 15:15:25 +0300

    Initial Commit

---
+++ SwiftUI-Appetizers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Ермоленко Константин on 17.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Appetizers/SwiftUI_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersApp.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Ермоленко Константин on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
